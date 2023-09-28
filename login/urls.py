5184729f813f595ebf7cffd23bdc9dfb02940192
elianrugama <elianrugama200@gmail.com>
2023-09-28 13:08:46 -0600

    nuevo login UI , login escaneando QR

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
         path('accounts/', include('django.contrib.auth.urls')),
         path('logout/', views.logout, name='logout'),
         path('register/', views.register, name='register'),
+        path('codigo/<str:codigo>/', views.codigo, name='codigo'),
 
 ]
--- login/views.py
+++ login/views.py
@@ -11,16 +11,28 @@ def logout (request):
 def register (request):
     if request.method == 'POST':
         codigo = request.POST['codigo']
-        usuario = request.POST['email']
+        usuario = request.POST['username']
         contraseña = request.POST['contraseña']
+        email = request.POST['email']
+
         if Codigo.objects.filter(codigo=codigo).exists():
             return JsonResponse({'error': 'El codigo ya existe'})
         else:
             nuevoCodigo = Codigo(codigo=codigo, usuario=usuario, contraseña=contraseña)
             nuevoCodigo.save()
-            nuevoUsuario = User.objects.create_user(username=usuario, password=contraseña)
+            nuevoUsuario = User.objects.create_user(username=usuario, password=contraseña, email=email)
             nuevoUsuario.save()
-            return JsonResponse({'success': 'El codigo se ha registrado correctamente'})
+            return JsonResponse({'success': 'El codigo se ha registrado correctamente', "usuario": usuario, "contraseña": contraseña})
         
     else:
         return redirect('/')
+    
+
+def codigo (request, codigo):
+    if Codigo.objects.filter(codigo=codigo).exists():
+        codigo = Codigo.objects.get(codigo=codigo)
+        return JsonResponse({'success': 'El codigo existe', "usuario": codigo.usuario, "contraseña": codigo.contraseña})
+    else:
+        return JsonResponse({'error': 'El codigo no existe'})
+    
+
